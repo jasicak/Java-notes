042c458ddb475aac6ebdd3cb1f85ed273303a56e
Jasica Kaur <119054829+jasicak@users.noreply.github.com>
2025-03-11 11:44:34 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,26 @@
+class Hello{                            // class name is Hello because file name is Hello.java
+    public static void main(String args[]){     // the main method is the entry point into the application
+                                                // void is the return type that function will return
+                                                //static enables us to return the value without creating a object and directly from the class
+        //if u use 'println' outcome will be printed in next line
+        // if u use 'print' output will be printed in the same line.
+        System.out.println("Hello World");
+        System.out.print("Hello World");
+        System.out.println("Hello World");
+    }
+}
+
+
+// for classes we use PascalConvention i.e.  1st letter is capital
+// for functions we use camelCaseConvention i.e. first letter small followed by capital letters for next words without giving space.
+
+// AddTwoNumbers --> pascal naming convention
+// addTwoNumber --> camel case convention
+
+// You can add comments i.e. kind of notes using double //
+// while naming spaces is not allowed
+
+
+// PACKAGE: Is used to group related classes and functions
+//FUNCTION: Public static void main is the function here in which operation is defined and can be called for n number of times
+//CLASSES: A collection of similar kind of functions
